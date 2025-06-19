66b68d2ce56302351aade750afc8d066b3a8898f
yunzzng <leejiyun0114@naver.com>
2025-06-19 14:53:38 +0900

    update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import optimizeImagePlugin from "vite-plugin-optimize-image";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), optimizeImagePlugin()],
+  plugins: [react()],
 })
